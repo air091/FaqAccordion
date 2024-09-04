dda9b89712f01e88983d8bb28c1495f79fc60819
air091 <141595375+air091@users.noreply.github.com>
2024-09-05 01:48:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.question').forEach(question => {
+  question.addEventListener('click', function() {
+    const section = this.parentElement;
+    const img = this.querySelector('img');
+
+    section.classList.toggle('active');
+
+    if (section.classList.contains('active')) {
+      img.src = '/FAQ_accordion/assets/icon-minus.svg';
+    } else {
+      img.src = '/FAQ_accordion/assets/icon-plus.svg';
+    }
+
+    document.querySelectorAll('section').forEach(currentSection => {
+      if (currentSection !== section) {
+        currentSection.classList.remove('active');
+        currentSection.querySelector('img').src = '/FAQ_accordion/assets/icon-plus.svg';
+      }
+    });
+
+  });
+  
+});
